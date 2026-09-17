0b302775feac3261b2e9ca29fde3ae278fc1d729
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:17:00 +0530

    Update TestCase.php

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -27,7 +27,7 @@ abstract class TestCase extends BaseTestCase
         $this->artisan('migrate', ['--database' => 'testing', '--force' => true]);
     }
     
-    protected function artisan($command, array $parameters = [])
+    public function artisan($command, array $parameters = [])
     {
         $kernel = $this->app->make(\Illuminate\Contracts\Console\Kernel::class);
         return $kernel->call($command, $parameters);
