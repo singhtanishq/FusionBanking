c430a5b508d201d6ee3e411ff9fcf85f6c55cb4d
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:39:51 +0530

    Update CustomerFactory.php

--- backend/database/factories/CustomerFactory.php
+++ backend/database/factories/CustomerFactory.php
@@ -13,10 +13,10 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
         return [
-            'customer_id' => 'CUS' . $this->faker->unique()->numerify('#######'),
+            'customer_id' => 'CUS' . $this->faker->unique()->numberBetween(1000000, 9999999),
             'email' => $this->faker->unique()->safeEmail(),
-            'mobile' => '9' . $this->faker->numerify('#########'),
-            'alternate_mobile' => '8' . $this->faker->numerify('#########'),
+            'mobile' => '9' . $this->faker->unique()->numberBetween(100000000, 999999999),
+            'alternate_mobile' => '8' . $this->faker->numberBetween(100000000, 999999999),
             'full_name' => $this->faker->name(),
             'father_name' => $this->faker->name('male'),
             'mother_name' => $this->faker->name('female'),
@@ -27,7 +27,7 @@ class CustomerFactory extends Factory
             'occupation' => $this->faker->jobTitle(),
             'annual_income' => $this->faker->numberBetween(300000, 5000000),
             'pan_number' => strtoupper($this->faker->bothify('?????####?')),
-            'aadhaar_number' => $this->faker->numerify('############'),
+            'aadhaar_number' => $this->faker->numberBetween(100000000000, 999999999999),
             'kyc_type' => 'full',
             'kyc_verified_at' => now(),
             'password' => bcrypt('password123'),
