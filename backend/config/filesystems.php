209fdb86ba46085cf42aa29911066a1b25ac8b71
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-17 13:52:24 +0530

    Update filesystems.php

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -2,7 +2,19 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    */
+
     'default' => env('FILESYSTEM_DISK', 'local'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    */
 
     'disks' => [
 
@@ -10,7 +22,6 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
             'throw' => false,
-            'report' => false,
         ],
 
         'public' => [
@@ -19,7 +30,6 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
@@ -32,17 +42,23 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
-            'report' => false,
         ],
 
         'private' => [
             'driver' => 'local',
             'root' => storage_path('app/private'),
+            'visibility' => 'private',
             'throw' => false,
             'report' => false,
         ],
 
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    */
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
