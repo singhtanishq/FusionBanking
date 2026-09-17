c67eaaa47a7099bdd2945aa5ea9f2ac398b6afc8
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:55:34 +0530

    Add BeneficiaryFactory.php

---
+++ backend/database/factories/BeneficiaryFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Beneficiary;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BeneficiaryFactory extends Factory
+{
+    protected $model = Beneficiary::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'account_number' => '50' . $this->faker->unique()->numberBetween(1000000000, 9999999999),
+            'ifsc_code' => 'FUSB0001001',
+            'nickname' => $this->faker->name(),
+            'is_verified' => $this->faker->boolean(),
+        ];
+    }
+}
