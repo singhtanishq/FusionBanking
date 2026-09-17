02219f5627016a735a5a39bccd1fb14651c8299a
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-17 13:49:15 +0530

    Add queue.php

---
+++ backend/config/queue.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    'default' => env('QUEUE_CONNECTION', 'database'),
+
+    'connections' => [
+
+        'sync' => [
+            'driver' => 'sync',
+        ],
+
+        'database' => [
+            'driver' => 'database',
+            'table' => 'jobs',
+            'queue' => 'default',
+            'retry_after' => 90,
+            'after_commit' => false,
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+            'queue' => env('REDIS_QUEUE', 'default'),
+            'retry_after' => 90,
+            'block_for' => null,
+            'after_commit' => false,
+        ],
+
+    ],
+
+    'batching' => [
+        'database' => 'database',
+        'table' => 'job_batches',
+    ],
+
+    'failed' => [
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'table' => 'failed_jobs',
+    ],
+
+];
