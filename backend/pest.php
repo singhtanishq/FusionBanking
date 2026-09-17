7e547ee82cfa87c1a1b47ae49546d899001d68fb
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:14:29 +0530

    Update pest.php

--- backend/pest.php
+++ backend/pest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Config;
 
 Config::set('database.default', 'testing');
@@ -14,6 +15,10 @@ require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
+
+// Set up facade root
+Facade::clearResolvedInstances();
+Facade::setFacadeApplication($app);
 
 // Run migrations for testing
 $app->make('Illuminate\Contracts\Console\Kernel')->call('migrate', ['--force' => true, '--database' => 'testing']);
