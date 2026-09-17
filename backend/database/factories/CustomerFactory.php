ac52c8a3c7c23b40865eaf157c9a23ea04b08578
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:42:27 +0530

    Update CustomerFactory.php

--- backend/database/factories/CustomerFactory.php
+++ backend/database/factories/CustomerFactory.php
@@ -14,7 +14,7 @@ class CustomerFactory extends Factory
     {
         return [
             'customer_id' => 'CUS' . $this->faker->unique()->numberBetween(1000000, 9999999),
-            'email' => $this->faker->unique()->email(),
+            'email' => $this->faker->unique()->safeEmail(),
             'mobile' => '9' . $this->faker->unique()->numberBetween(100000000, 999999999),
             'alternate_mobile' => '8' . $this->faker->numberBetween(100000000, 999999999),
             'full_name' => $this->faker->name(),
