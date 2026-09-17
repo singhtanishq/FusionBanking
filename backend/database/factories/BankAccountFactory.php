6332635ed279cc4f795cde0b729b382030c95f23
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-17 13:51:51 +0530

    Add BankAccountFactory.php

---
+++ backend/database/factories/BankAccountFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BankAccount;
+use App\Enums\AccountStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BankAccountFactory extends Factory
+{
+    protected $model = BankAccount::class;
+
+    public function definition(): array
+    {
+        return [
+            'account_number' => '50' . $this->faker->unique()->numerify('##########'),
+            'ifsc_code' => 'FUSB0001001',
+            'account_type' => $this->faker->randomElement(['savings', 'current']),
+            'status' => AccountStatus::ACTIVE,
+            'balance' => $this->faker->randomFloat(2, 0, 1000000),
+            'available_balance' => $this->faker->randomFloat(2, 0, 1000000),
+            'opening_date' => $this->faker->dateTimeBetween('-2 years', 'now'),
+            'is_primary' => true,
+        ];
+    }
+}
