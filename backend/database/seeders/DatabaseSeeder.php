4994793654917ed5d028de8fb920387959344f51
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:05:20 +0530

    Update multiple files

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
-use App\Models\Role;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Models\SystemSetting;
 use App\Models\LoanProduct;
