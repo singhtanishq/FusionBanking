f3b0e0f68124ad19ef945523c33136da6dfdc1fb
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 01:20:03 +0530

    Update TestCase.php

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
-abstract class TestCase extends BaseTestCase
+class TestCase extends BaseTestCase
 {
     use RefreshDatabase, WithFaker;
 
