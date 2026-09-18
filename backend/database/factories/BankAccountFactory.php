f9605f50a4124051fb811e74c45925e1a30a98b6
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 13:02:50 +0530

    Update BankAccountFactory.php

--- backend/database/factories/BankAccountFactory.php
+++ backend/database/factories/BankAccountFactory.php
@@ -17,7 +17,7 @@ class BankAccountFactory extends Factory
         return [
             'account_number' => '50' . $this->faker->unique()->numberBetween(1000000000, 9999999999),
             'ifsc_code' => 'FUSB0001001',
-            'account_type' => $accountTypes[$this->faker->numberBetween(0, 1)],
+            'account_type' => $this->faker->randomElement($accountTypes),
             'status' => AccountStatus::ACTIVE,
             'balance' => $this->faker->randomFloat(2, 0, 1000000),
             'available_balance' => $this->faker->randomFloat(2, 0, 1000000),
