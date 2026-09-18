043cca8a02d11a4d76f22775924da821a885d800
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 12:20:36 +0530

    Update BeneficiaryFactory.php

--- backend/database/factories/BeneficiaryFactory.php
+++ backend/database/factories/BeneficiaryFactory.php
@@ -12,10 +12,10 @@ class BeneficiaryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
             'account_number' => '50' . $this->faker->unique()->numberBetween(1000000000, 9999999999),
             'ifsc_code' => 'FUSB0001001',
-            'nickname' => $this->faker->name(),
+            'nickname' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
             'is_verified' => $this->faker->boolean(),
         ];
     }
