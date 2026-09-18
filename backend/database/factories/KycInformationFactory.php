1aadb65b4280602e4aa95cdc063b4acd82646295
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 15:59:44 +0530

    Update KycInformationFactory.php

--- backend/database/factories/KycInformationFactory.php
+++ backend/database/factories/KycInformationFactory.php
@@ -11,8 +11,8 @@ class KycInformationFactory extends Factory
 
     public function definition(): array
     {
-        // Generate PAN using bothify format
-        $pan = strtoupper($this->faker->format('bothify', ['?????####?']));
+        // Generate PAN using bothify method call
+        $pan = strtoupper($this->faker->bothify('?????####?'));
         
         return [
             'pan_number' => $pan,
