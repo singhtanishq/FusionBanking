aad1e1bd10bfa23a68134993fc3815766c57b14d
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:04:48 +0530

    Update Loan.php

--- backend/app/Models/Loan.php
+++ backend/app/Models/Loan.php
@@ -71,7 +71,7 @@ class Loan extends Model
 
     public function account(): BelongsTo
     {
-        return $this->belongsTo(BankAccount::class);
+        return $this->belongsTo(BankAccount::class, 'account_id');
     }
 
     public function product(): BelongsTo
