1587f343198271b53d2233947cfb3e012c002c24
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 12:35:30 +0530

    Update TestCase.php

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    use \Illuminate\Foundation\Testing\RefreshDatabase, \Illuminate\Foundation\Testing\WithFaker;
+    use \Illuminate\Foundation\Testing\WithFaker;
 
     protected function setUp(): void
     {
@@ -19,6 +19,9 @@ class TestCase extends BaseTestCase
             'database' => __DIR__ . '/../storage/testing.sqlite',
             'prefix' => '',
         ]);
+        
+        // Run migrations once per test class
+        $this->artisan('migrate', ['--database' => 'testing', '--force' => true]);
     }
     
     public function artisan($command, $parameters = [])
