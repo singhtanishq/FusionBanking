5b69731f333283dff350284bb3f36337ba9c1818
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 11:46:23 +0530

    Update TestCase.php

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    use \Illuminate\Foundation\Testing\RefreshDatabase, \Illuminate\Foundation\Testing\WithFaker;
+    use \Illuminate\Foundation\Testing\WithFaker;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
         
